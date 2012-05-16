2abee044b3fd280aae2209545d59a6bd1a865256
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-05-16 15:52:46 +0200

    Bugfix in negation on lazy grounder.

--- src/modules/LazyGrounder.cpp
+++ src/modules/LazyGrounder.cpp
@@ -236,7 +236,7 @@ bool LazyTseitinClause::checkPropagation(Implication& tocheck, bool signswapped,
 				if (waseq) {
 					for (auto i = newgrounding.cbegin(); i < newgrounding.cend(); ++i) {
 						//cerr <<"Adding constraint" <<long(this) <<"\n";
-						internalAdd(Disjunction( { not complement.head, not *i }), getPCSolver());
+						internalAdd(Disjunction( { not complement.head, signswapped?*i:not *i}), getPCSolver());
 						//cerr <<"Finished constraint" <<long(this) <<"\n";
 					}
 				}
